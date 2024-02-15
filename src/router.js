2b863209b147512626b8f87adab645bf4288eb17
Nilceg <ngarc289@stu.southtexascollege.edu>
2024-02-14 22:50:47 -0600

    updated nav bar

--- src/router.js
+++ src/router.js
@@ -25,7 +25,7 @@ const router = createRouter({
 //navigation guard
 router.beforeEach((to, _, next) => {
   if (to.meta.requiresAuth && !isAuthenticated.value) {
-    next({name: 'LoginPage', query: {redirect: toString.fullPath}})
+    next({name: 'LoginPage', query: {redirect: to.fullPath}})
   } else {
     next()
   }
